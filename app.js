152aaf4fd9158c8106951ad376278f0bb969238a
Johannes Lamberts <mail@j-lamberts.de>
2019-01-19 00:28:04 +0100

    1 - computed

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ Vue.component('color-form', {
       :style="style"
       class="color-container"
     >
+        <v-select
+          :items="directions"
+          label="Direction"
+          v-model="direction"
+        />
         <p>{{style.backgroundImage}}</p>
     </div>
     `,
   data() {
     return {
-      style: {
-        backgroundImage: `linear-gradient(to right, hsl(0, 70%, 50%), hsl(100, 70%, 50%))`,
+      direction: 'to right',
+      directions: ['to top', 'to right', 'to bottom', 'to left'],
+    }
+  },
+  computed: {
+    style() {
+      return {
+        backgroundImage: `linear-gradient(${this.direction}, hsl(0, 70%, 50%), hsl(100, 70%, 50%))`,
         padding: '30px',
         margin: '16px 0',
-      },
-    }
+      }
+    },
   },
 })
 
